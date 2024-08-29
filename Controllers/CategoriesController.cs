b7bc070bac3a0d8cc38a982709631c6b2b4d8db6
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-08-29 20:48:47 +0300

    added database tabels

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
